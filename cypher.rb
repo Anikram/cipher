74aa51177291d9c4e09257a687e9d0066c012749
Anikram <4nikram@gmail.com>
2018-03-21 23:23:09 +0300

    sha2 ready

--- cypher.rb
+++ cypher.rb
@@ -1,13 +1,15 @@
-require 'digest'
 require_relative 'methods.rb'
 
 puts 'Введите фразу для шифрования:'
-phrase = gets.chomp
+phrase = STDIN.gets.chomp
 
 puts 'Каким методом зашифровать данные:
 1. MD5
-2. SHA1 ?'
+2. SHA1
+3. SHA2?'
+
 choice = 0
-choice = gets.to_i until [1, 2].include?(choice)
+choice = gets.to_i until [1, 2, 3].include?(choice)
 puts 'Результат шифрования:'
+
 puts cipher_string(phrase, choice)
--- methods.rb
+++ methods.rb
@@ -1,5 +1,8 @@
+require 'digest'
+
 def cipher_string(phrase, method)
   encrypted = Digest::MD5.hexdigest phrase if method == 1
   encrypted = Digest::SHA1.hexdigest phrase if method == 2
+  encrypted = Digest::SHA2.hexdigest phrase if method == 3
   encrypted
 end
